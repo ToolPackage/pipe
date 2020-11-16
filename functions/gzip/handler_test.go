c89f0929da23c1f07921fea998e7093989572171
I507145 <jingwei.li@sap.com>
2020-11-16 11:34:29 +0800

    .

--- functions/gzip/handler_test.go
+++ functions/gzip/handler_test.go
@@ -10,9 +10,9 @@ import (
 func TestGzip(t *testing.T) {
 	data := "asd"
 	buf := new(strings.Builder)
-	_ = Compress(nil, bytes.NewReader([]byte(data)), buf)
+	_ = compress(nil, bytes.NewReader([]byte(data)), buf)
 	compressedData := []byte(buf.String())
 	buf = new(strings.Builder)
-	_ = Decompress(nil, bytes.NewReader(compressedData), buf)
+	_ = decompress(nil, bytes.NewReader(compressedData), buf)
 	assert.Equal(t, data, buf.String())
 }
